a7211604ea5cc0407ec9119d6c411bc43573dc6a
indusclass <164315589+indusclass@users.noreply.github.com>
2024-03-21 13:18:07 +0530

    Add files via upload

---
+++ pal.py
@@ -0,0 +1,11 @@
+st=input("Enter the string:")
+
+pal=True
+h=len(st)//2
+for i in range (0,h):
+    if st[i]!=st[-i-1]:
+        pal=False
+if pal:
+    print("The given string is a palindrome")
+else:
+    print("The given string is not a palindrome")
